b79eb4c31f8167d3251319afdaa433d2607cb8e0
Lexie <Lexie_Li@outlook.com>
2021-04-23 16:51:55 +0800

    初步实现了路由

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    This is the Login page.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    This is the register page.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
